dd2b317f1d19937b3d55f12d5205f5270cbf8758
kamiko <2966569584@qq.com>
2019-12-19 10:31:21 +0800

    idea测试

--- src/main/java/com/iuv/service/MovieSeatService.java
+++ src/main/java/com/iuv/service/MovieSeatService.java
@@ -3,11 +3,14 @@ package com.iuv.service;
 import java.util.List;
 import java.util.Map;
 
+import com.iuv.dao.MovieSeatDao;
 import com.iuv.pojo.movie.Movie;
+import com.iuv.pojo.movie.MovieScene;
 
-public interface MovieSeatService {
 /**
  * 选座
- * 
+ *
  * */
+public interface MovieSeatService {
+    List<MovieScene> getScenes(Integer movieId);
 }
