f45396767b67d7fe44f357ade7104514685b5b3f
kamiko <2966569584@qq.com>
2019-12-20 12:09:03 +0800

    买票js

--- src/main/resources/static/js/chooseSeat/ChooseSeat.js
+++ src/main/resources/static/js/chooseSeat/ChooseSeat.js
@@ -3,7 +3,9 @@ $(function() {
         el:'.m2',
         data:{
             movieMsg:{},
-            sceneMsg:{}
+            sceneMsg:{},
+            falg:[],
+            nums:[],
         },
         methods:{
 
@@ -11,7 +13,35 @@ $(function() {
     });
     AjaxMovieMsg(movie);
     AjaxSceneMsg(movie);
+    giveNum(movie);
+    someChangs();
 });
+
+function someChangs() {
+
+}
+
+function giveNum(movie) {
+    $("#text span").each(function (index,val) {
+        $(val).attr("num",index+1);
+        movie.falg.push(false);
+        $(val).click(function () {
+            movie.falg[index] = !movie.falg[index];
+            var flag = movie.falg[index];
+            var mun = $(val).attr("num")-0;
+            if(flag){
+                $(val).attr("class","seat selected")
+                movie.nums.push(mun);
+                console.log(movie.nums)
+            }else{
+                $(val).attr("class","seat selectable");
+                movie.nums.splice(movie.nums.indexOf(mun),1);
+                console.log(movie.nums)
+            }
+        });
+
+    })
+}
 
 function AjaxMovieMsg(movie) {
     var url = "/ticket/getMsg";
